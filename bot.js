935bd08f3e85b72b09f8ca72bc18f9ddbcd061f0
Ivandelg0204 <60906108+Ivandelg0204@users.noreply.github.com>
2026-03-09 12:45:31 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer");
+const axios = require("axios");
+
+const N8N_WEBHOOK = "https://n8n-n8n.owlzof.easypanel.host/webhook/messenger";
+
+(async () => {
+
+const browser = await puppeteer.launch({
+    headless:false,
+    args:["--no-sandbox"]
+});
+
+const page = await browser.newPage();
+
+await page.goto("https://www.facebook.com/messages");
+
+console.log("Inicia sesión manualmente...");
+
+await page.waitForSelector('[role="main"]');
+
+console.log("Messenger cargado");
+
+setInterval(async ()=>{
+
+const messages = await page.evaluate(()=>{
+
+const chats = document.querySelectorAll('[data-testid="mwthreadlist-item"]');
+
+let data = [];
+
+chats.forEach(chat=>{
+
+let name = chat.innerText;
+
+data.push(name);
+
+});
+
+return data;
+
+});
+
+for(const msg of messages){
+
+await axios.post(N8N_WEBHOOK,{
+message:msg
+});
+
+console.log("Mensaje enviado a n8n:",msg);
+
+}
+
+},5000);
+
+})();
